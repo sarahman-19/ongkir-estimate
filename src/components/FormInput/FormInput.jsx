5c3473529744a7f56ac1dbccbbb32df606dda88c
sarahman21 <rahman.js2021@gmail.com>
2021-11-19 23:05:02 +0800

    change button style

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,4 +1,4 @@
-import { FormGroup, Input, Label } from "reactstrap";
+import { FormGroup, Input, Label, Button } from "reactstrap";
 import { React, Component } from "react";
 import "./FormInput.css";
 
@@ -30,11 +30,21 @@ class FormInput extends Component {
             <div className="input">
               <Label for="asal">Asal</Label>
               <Input id="asal" name="asal" type="select">
-                <option value='-0.4510928, 117.1437407' selected>AWS</option>
-                <option value='-0.4998679, 117.1303437' selected>KMD</option>
-                <option value='-0.482450, 117.163399' selected>PLT</option>
-                <option value='-0.5072819, 117.1692535' selected>SBT</option>
-                <option value='-0.4759319, 117.1651373' selected>RMJ</option>
+                <option value="-0.4510928, 117.1437407" selected>
+                  AWS
+                </option>
+                <option value="-0.4998679, 117.1303437" selected>
+                  KMD
+                </option>
+                <option value="-0.482450, 117.163399" selected>
+                  PLT
+                </option>
+                <option value="-0.5072819, 117.1692535" selected>
+                  SBT
+                </option>
+                <option value="-0.4759319, 117.1651373" selected>
+                  RMJ
+                </option>
               </Input>
             </div>
             <div className="input">
@@ -49,16 +59,18 @@ class FormInput extends Component {
               />
             </div>
             <div>
-              <Input
+              <Button
                 onClick={() =>
                   this.props.handleSubmit(
-                    document.getElementById('asal').value,
+                    document.getElementById("asal").value,
                     this.state.data.tujuan
                   )
                 }
-                type="submit"
-                value="Estimate"
-              />
+                block
+                color="primary"
+              >
+                Estimate
+              </Button>
             </div>
           </FormGroup>
         </div>
